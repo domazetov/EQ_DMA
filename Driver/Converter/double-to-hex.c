c639bfc9f1be118e5e167becaec1e5ec0e7cb35e
domazetov <nebojsadomazetov@gmail.com>
2020-07-08 03:44:18 +0200

    new amps

--- Driver/Converter/double-to-hex.c
+++ Driver/Converter/double-to-hex.c
@@ -10,20 +10,77 @@ int convert(double val)
 
 int main()
 {
+    FILE *fa;
     FILE *fp;
+    FILE *fpr;
     FILE *fo;
+
+    double amps[10];
+    int boundaries[9];
+
     double number[SONG_LENGTH];
     int result[SONG_LENGTH];
     int i, k = 0;
 
-    fp = fopen("audiodouble.txt", "r");
+    fp = fopen("amps.txt", "r");
+    for (i = 0; i < 10; i++)
+    {
+        fscanf(fp, "%lf", &amps[i]);
+    }
+    fclose(fp);
+
+    fa = fopen("audiodouble.txt", "r");
     for (i = 0; i < SONG_LENGTH; i++)
     {
         fscanf(fp, "%lf", &number[i]);
     }
-    fclose(fp);
+    fclose(fa);
 
-    fo = fopen("../audio_app/audiohex.h", "w");
+    fpr = fopen("freq.txt", "r");
+    for (i = 0; i < 9; i++)
+    {
+        fscanf(fp, "%d", &boundaries[i]);
+    }
+    fclose(fpr);
+
+    fo = fopen("../audio_app/coeficients.h", "w");
+
+    fprintf(fo, "unsigned int p[] = \n{\n");
+    for (i = 0; i < 10; i++)
+    {
+        result[i] = convert(amps[i]);
+        fprintf(fo, "%#0010x", result[i]);
+        if (i != 10 - 1)
+        {
+            fprintf(fo, ",");
+            k++;
+            if (k == 5)
+            {
+                k = 0;
+                fprintf(fo, "\n");
+            }
+        }
+    }
+    k = 0;
+    fprintf(fo, "};\n\n\n");
+
+    fprintf(fo, "unsigned int pr[] = \n{\n");
+    for (i = 0; i < 9; i++)
+    {
+        fprintf(fo, "%#0010x", boundaries[i]);
+        if (i != 10 - 1)
+        {
+            fprintf(fo, ",");
+            k++;
+            if (k == 5)
+            {
+                k = 0;
+                fprintf(fo, "\n");
+            }
+        }
+    }
+    k = 0;
+    fprintf(fo, "};\n\n\n");
 
     fprintf(fo, "unsigned int audio[] = \n{\n");
     for (i = 0; i < SONG_LENGTH; i++)
@@ -40,7 +97,6 @@ int main()
                 fprintf(fo, "\n");
             }
         }
-        //printf("Double: %.10lf     Int: %#10x\n", number[i], result[i]);
     }
     fprintf(fo, "};\n");
     fclose(fo);
--- Driver/audio_app/app.c
+++ Driver/audio_app/app.c
@@ -1,4 +1,3 @@
-#include "audiohex.h"
 #include "coeficients.h"
 
 #include <stdio.h>
